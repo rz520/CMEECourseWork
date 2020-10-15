091147c57574b145c4db6290cc1bc6312b710152
rz520 <rui.zhang20@imperial.ac.uk>
2020-10-15 15:30:00 +0800

    removed the file

--- Week1/code/tabtocsv.sh
+++ Week1/code/tabtocsv.sh
@@ -7,7 +7,21 @@
 # Arguments: 1 -> tab delimited file
 # Date: Oct 2020
 
-echo "Creating a comma delimited version of $1 ..."
-cat $1 | tr -s "\t" "," >> $1.csv
-echo "Done!"
-exit
+if [ -n "$1" ]    # please give a relative path to $1 when use this shell script 
+then
+    if [ -e $1 ]
+    then
+        if [ $(grep -o "\t" $1 | wc -l) != 0 ]
+        then
+            echo "Creating a comma delimited version of $1 ..."
+            cat $1 | tr -s "\t" "," >> $1.csv
+            echo "Done!"
+        else
+            echo "$1 is not a tab delimited file"
+        fi
+    else
+        echo "$1 does not exist"
+    fi
+else 
+   echo "There is no input file"
+fi 
--- Week1/code/variables.sh
+++ Week1/code/variables.sh
@@ -1,6 +1,19 @@
-#
+#!/bin/bash
+# Author: Rui rui.zhang20@imperial.ac.uk
+# Script: variables.sh
+# Description: practice 3 ways to assign values to variables in shell scripts
+# Date: Oct 2020
 
+# Shows the use of  variables
+MyVar='some string'
+echo 'the current value of the variable is' $MyVar
+echo 'Please enter a new string'
+read MyVar
+echo 'the current value of the variable is' $MyVar
 
+## Reading multiple values
+echo 'Enter two numbers separated by space(s)'
+read a b
 echo 'you entered' $a 'and' $b '. Their sum is :'
-mysum= `expr $a +$b`
+mysum=`expr $a + $b`
 echo $mysum
